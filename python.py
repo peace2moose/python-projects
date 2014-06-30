a3772884687c3f24492e1c43ab113b8f1fae4161
Daytonious the Great <peace2moose@gmail.com>
2014-06-30 16:58:37 -0400

    i made a computer talk to me

---
+++ python.py
@@ -0,0 +1,14 @@
+answer = input ("what is your name?")
+name = "Dayton"
+print("hello" + answer)
+
+age = "11"
+
+answer = input ("what is your age?")
+print("age: " + str(answer))
+#boolian example
+happy = True
+answer = input ("are you happy?")
+print("happy: " + str(answer))
+
+
