083a9afcbbcd51019569fa31e6ed898408c81548
Daytonious the Great <peace2moose@gmail.com>
2014-07-14 12:29:49 -0400

    Update age in days.py

--- age in days.py
+++ age in days.py
@@ -1,14 +1,10 @@
+print ("hi user, i am your computer. i dont know much about you.")
 name = input ("what is your name?")
+print ("hello " + name + " you are looking lovely today!")
 age = input ("how old are you?")
 age = int(11)
 your_age = input ("you are " + str( int(age) * 365) + " days old")
-
-password = input ("what is the password?").strip().lower()
-if name == "25593":
-    print ("awsome!")
-else:
-    print("DENIED! intruder found.")
-    exit()
 print (" hello there " + name)
 print (" you are " + name + " and you are " + age + " years old")
+frend = input ("who is your best frends name?")
 
